e005a49bb730a68155d48093b3edb8e56a2be0f6
caulutes <79812622+caulutes@users.noreply.github.com>
2022-01-31 21:09:09 -0300

    Update 3.sh

--- configs/arch/3.sh
+++ configs/arch/3.sh
@@ -10,10 +10,10 @@ cd yay-git
 makepkg -si
 
 # Instalando aplicações de sistema básicas
-sudo pacman -Syy xf86-video-intel xf86-input-synaptics xorg bspwm sxhkd lightdm lightdm-gtk-greeter alacritty rofi arandr feh zsh hsetroot xdo snes9x-gtk virtualbox ruby; yay -Syy google-chrome picom-jonaburg-git polybar jstest-gtk-git 
+sudo pacman -Syy xf86-video-intel xf86-input-synaptics xorg bspwm sxhkd lightdm lightdm-gtk-greeter alacritty rofi arandr feh zsh hsetroot xdo snes9x-gtk virtualbox ruby unclutter; yay -Syy google-chrome picom-jonaburg-git polybar jstest-gtk-git 
 
 # Instalando fontes 
 yay -S otf-fantasque-sans-mono nerd-fonts-git ipa-fonts cozzete-ttf ttf-roboto-mono otf-font-awesome
 
 # Copiando arquivos de configuração
-sudo cp -r ~/configs2/configs/bspwm ~/.config; sudo cp -r ~/configs2/configs/sxhkd ~/.config; sudo cp -r ~/configs2/configs/alacritty ~/.config; sudo cp -r ~/configs2/configs/picom ~/.config; sudo cp -r ~/configs2/configs/polybar ~/.config
+sudo cp -r ~/configs/configs/bspwm ~/.config; sudo cp -r ~/configs/configs/sxhkd ~/.config; sudo cp -r ~/configs/configs/alacritty ~/.config; sudo cp -r ~/configs/configs/picom ~/.config; sudo cp -r ~/configs/configs/polybar ~/.config
